9b2ed8416d7d28382506f575f02b5109315595f9
Yadu-krishnankv <yadhumanoj12@gmail.com>
2025-04-23 01:02:42 +0530

    Fix-paymentResponseTrimspace

--- Server/Controllers/paymentController.js
+++ Server/Controllers/paymentController.js
@@ -70,9 +70,9 @@ const paymentResponse = async (req, res) => {
 
       const subscription = new Subscription({
         orderId: reqData['order_id'],
-        templeId: reqData[' address_line_2'],
+        templeId: reqData['address_line_2'],
         templeName: reqData['name'],
-        address: reqData[' address_line_1'],
+        address: reqData['address_line_1'],
         email: reqData['email'],
         number: reqData['phone'],
         amount: parseFloat(reqData['amount']),
